85daab7f3d102bfc41fde8401c26e91e875b3aa4
Vivek <ojhavivek24@gmail.com>
2025-02-24 12:19:15 +0530

    added middlewares

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
 import express from 'express';
-
-
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials:true,
+    origin: "http://localhost:5173"
+}))
 
 app.get("/",(req,res) => {
-    res.send("")
+    res.json({msg:"Server is healthy"})
 })
 
 
