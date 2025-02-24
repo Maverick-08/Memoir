428414c2b62ac3988171b3bcf6d64c4ee2a8c00f
Vivek <ojhavivek24@gmail.com>
2025-02-24 11:37:00 +0530

    added backend template

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res) => {
+    res.send("")
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server is running")
+})
