d93466c42436b61f48585853d062865905306ca9
Vivek <ojhavivek24@gmail.com>
2025-03-27 23:04:07 +0530

    setting secure to true

--- server/src/controllers/userAuthentication.ts
+++ server/src/controllers/userAuthentication.ts
@@ -87,8 +87,8 @@ export const userAuthenticationHandler = async (
     // Production : {httpOnly:true,sameSite:"none",secure:true,maxAge:24*60*60*1000}
     res.cookie("__authCookie__", __authCookie__, {
       httpOnly: true,
-      sameSite: "none",
       secure: true,
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
